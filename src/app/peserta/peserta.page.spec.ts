6c240bd9cf76f270790b4af2732e118284e7833d
RadyaNala <hiradyanala11@gmail.com>
2023-12-01 19:29:55 +0700

    fix import

--- src/app/peserta/peserta.page.spec.ts
+++ src/app/peserta/peserta.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { PesertaPage } from './peserta.page';
 
 describe('PesertaPage', () => {
